c68afb0a59e93ceae035c6746ac5a439d69749b5
Haixin <haixinhuang502@gmail.com>
2026-04-01 15:41:14 -0700

    feat: created form

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,44 +1,29 @@
 // src/MyApp.jsx
 import React, { useState } from "react";
 import Table from "./Table";
+import Form from "./Form";
 
-const characters = [
-  {
-    name: "Charlie",
-    job: "Janitor"
-  },
-  {
-    name: "Mac",
-    job: "Bouncer"
-  },
-  {
-    name: "Dee",
-    job: "Aspring actress"
-  },
-  {
-    name: "Dennis",
-    job: "Bartender"
-  }
-];
+// const characters = [
+//   {
+//     name: "Charlie",
+//     job: "Janitor"
+//   },
+//   {
+//     name: "Mac",
+//     job: "Bouncer"
+//   },
+//   {
+//     name: "Dee",
+//     job: "Aspring actress"
+//   },
+//   {
+//     name: "Dennis",
+//     job: "Bartender"
+//   }
+// ];
 
 function MyApp() {
-    const [characters, setCharacters] = useState([
-        {    name: "Charlie",
-            job: "Janitor"
-        },
-        {
-            name: "Mac",
-            job: "Bouncer"
-        },
-        {
-            name: "Dee",
-            job: "Aspring actress"
-        },
-        {
-            name: "Dennis",
-            job: "Bartender"
-        }
-        ]);
+    const [characters, setCharacters] = useState([]);
 
     function removeOneCharacter(index) {
         const updated = characters.filter((character, i) => {
@@ -52,6 +37,7 @@ function MyApp() {
       <Table
         characterData={characters} 
         removeCharacter={removeOneCharacter}/>
+        <Form />
     </div>
   );
 }
